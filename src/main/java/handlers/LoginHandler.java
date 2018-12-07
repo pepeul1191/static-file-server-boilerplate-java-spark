5130a219597ccd9bf13f4c1dd70acf649fa6eda6
Pepe Valdivia <jvaldivia@softweb.pe>
2018-12-07 16:31:17 -0500

    Login pero disabled

--- src/main/java/handlers/LoginHandler.java
+++ src/main/java/handlers/LoginHandler.java
@@ -19,7 +19,7 @@ public class LoginHandler {
     model.put("title", ConfigFactory.parseResources("contents/_titles.conf").getConfig("sp").getString("login_index"));
     model.put("content", ConfigFactory.parseResources("contents/login_index.conf").getConfig("sp"));
     model.put("constants", constants);
-    model.put("message", "");
+    model.put("message", ConfigFactory.parseResources("contents/login_index.conf").getConfig("sp").getString("message"));
     model.put("load_css", helper.indexCSS());
     model.put("load_js", helper.indexJS());
     return App.render("login/index.ftl.html", model);
